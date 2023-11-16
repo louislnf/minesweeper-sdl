4deabf73a95bef670bb14e8ff56ba99bb99302bf
Louis Lenief <lenief.louis@gmail.com>
2023-11-16 21:10:07 +0100

    Switch to using SDL_Renderer instead of SDL_Surface.

--- main.cc
+++ main.cc
@@ -4,11 +4,24 @@
 #include <SDL3/SDL_events.h>
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL_render.h>
 #include <SDL3/SDL_video.h>
 
 #include "game.h"
 
 namespace minesweeper {
+
+struct Color {
+  int r;
+  int g;
+  int b;
+
+  Color(int r, int g, int b) : r(r), g(g), b(b){};
+};
+
+void SetRendererDrawColor(SDL_Renderer* renderer, Color color) {
+  SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, SDL_ALPHA_OPAQUE);
+}
 
 class MinesweeperUI {
  public:
@@ -25,10 +38,9 @@ class MinesweeperUI {
       std::cout << "Failed to create window.";
       return false;
     }
-    pixel_format_ = SDL_CreatePixelFormat(SDL_GetWindowPixelFormat(window_));
-    surface_ = SDL_GetWindowSurface(window_);
-    if (surface_ == nullptr) {
-      std::cout << "Window surface is nullptr.";
+    renderer_ = SDL_CreateRenderer(window_, nullptr, 0);
+    if (renderer_ == nullptr) {
+      std::cout << "Failed to create renderer.";
       return false;
     }
     DrawMineField();
@@ -36,8 +48,8 @@ class MinesweeperUI {
   }
 
   void CleanUp() {
-    if (pixel_format_ != nullptr) {
-      SDL_DestroyPixelFormat(pixel_format_);
+    if (renderer_ != nullptr) {
+      SDL_DestroyRenderer(renderer_);
     }
     if (window_ != nullptr) {
       SDL_DestroyWindow(window_);
@@ -45,21 +57,23 @@ class MinesweeperUI {
   }
 
   void DrawMineField() {
+    SetRendererDrawColor(renderer_, Color(0, 0, 0));
+    SDL_RenderClear(renderer_);
     constexpr int cell_padding = 1;
     Minesweeper::FieldView field_view = game_.GetFieldView();
     for (int r = 0; r < field_view.size(); ++r) {
       for (int c = 0; c < field_view[r].size(); ++c) {
-        SDL_Rect rect{
+        SDL_FRect rect{
             .x = r * cell_size_ + cell_padding,
             .y = c * cell_size_ + cell_padding,
             .w = cell_size_ - 2 * cell_padding,
             .h = cell_size_ - 2 * cell_padding,
         };
-        SDL_FillSurfaceRect(surface_, &rect,
-                            GetColorForCellView(field_view[r][c]));
+        SetRendererDrawColor(renderer_, GetColorForCellView(field_view[r][c]));
+        SDL_RenderFillRect(renderer_, &rect);
       }
     }
-    SDL_UpdateWindowSurface(window_);
+    SDL_RenderPresent(renderer_);
   }
 
   void HandleLeftClick(int x, int y) {
@@ -82,27 +96,27 @@ class MinesweeperUI {
     DrawMineField();
   }
 
-  Uint32 GetColorForCellView(Minesweeper::CellView cell_view) {
+  Color GetColorForCellView(Minesweeper::CellView cell_view) {
     switch (cell_view.type) {
       case Minesweeper::CellType::kHidden:
         // Light grey.
-        return SDL_MapRGB(pixel_format_, 200, 200, 200);
+        return Color(200, 200, 200);
       case Minesweeper::CellType::kRevealed: {
         int neighbours = cell_view.neighbour_mines.has_value()
                              ? cell_view.neighbour_mines.value()
                              : 0;
         float coeff = 1.0 - neighbours / 8.0;
         // White but gets bluer as there are more neighbours.
-        return SDL_MapRGB(pixel_format_, 255 * coeff, 255 * coeff, 255);
+        return Color(255 * coeff, 255 * coeff, 255);
       }
       case Minesweeper::CellType::kFlagged:
         // Yellow.
-        return SDL_MapRGB(pixel_format_, 255, 255, 0);
+        return Color(255, 255, 0);
       case Minesweeper::CellType::kMined:
         // Red.
-        return SDL_MapRGB(pixel_format_, 255, 0, 0);
+        return Color(255, 0, 0);
     }
-    return 0;
+    return Color(0, 0, 0);
   }
 
  private:
@@ -111,8 +125,7 @@ class MinesweeperUI {
   int cell_size_;
   Minesweeper game_;
   SDL_Window* window_ = nullptr;
-  SDL_Surface* surface_ = nullptr;
-  SDL_PixelFormat* pixel_format_ = nullptr;
+  SDL_Renderer* renderer_ = nullptr;
 };
 
 }  // namespace minesweeper
