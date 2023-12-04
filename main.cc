23b303f9a9f30901b76c966c48ad1f57f712e903
Louis Lenief <lenief.louis@gmail.com>
2023-12-04 22:32:48 +0100

    Display number of neighbours.

--- main.cc
+++ main.cc
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
 #define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL_events.h>
@@ -6,6 +10,7 @@
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_video.h>
+#include <SDL3_ttf/SDL_ttf.h>
 
 #include "game.h"
 
@@ -33,21 +38,55 @@ class MinesweeperUI {
                                   3 * h / cell_size)) {}
 
   bool Initialize() {
+    font_ = TTF_OpenFont("C:/Windows/Fonts/arial.ttf", 30);
+    if (font_ == nullptr) {
+      std::cout << "Failed to load font" << std::endl;
+      return false;
+    }
     window_ = SDL_CreateWindow("Minesweeper", w_, h_, 0);
     if (window_ == nullptr) {
-      std::cout << "Failed to create window.";
+      std::cout << "Failed to create window." << std::endl;
       return false;
     }
     renderer_ = SDL_CreateRenderer(window_, nullptr, 0);
     if (renderer_ == nullptr) {
-      std::cout << "Failed to create renderer.";
+      std::cout << "Failed to create renderer." << std::endl;
+      return false;
+    }
+    if (!InitializeNumberTextures()) {
+      std::cout << "Failed to initilaze number textures." << std::endl;
       return false;
     }
     DrawMineField();
     return true;
   }
 
+  bool InitializeNumberTextures() {
+    std::cout << "Initializing number textures." << std::endl;
+    // Maximum number of neighbours is 8.
+    std::vector<std::string> texts{"1", "2", "3", "4", "5", "6", "7", "8"};
+    SDL_Color white{.r = 255, .g = 255, .b = 255, .a = SDL_ALPHA_OPAQUE};
+    for (int i = 0; i < texts.size(); ++i) {
+      std::cout << "Texture #" << i << std::endl;
+      SDL_Surface* surface =
+          TTF_RenderText_Solid(font_, texts[i].c_str(), white);
+      if (surface == nullptr) {
+        return false;
+      }
+      SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer_, surface);
+      if (texture == nullptr) {
+        return false;
+      }
+      SDL_DestroySurface(surface);
+      number_textures_.emplace(i + 1, texture);
+    }
+    return true;
+  }
+
   void CleanUp() {
+    if (font_ != nullptr) {
+      TTF_CloseFont(font_);
+    }
     if (renderer_ != nullptr) {
       SDL_DestroyRenderer(renderer_);
     }
@@ -57,9 +96,10 @@ class MinesweeperUI {
   }
 
   void DrawMineField() {
+    std::cout << "Drawing mine field" << std::endl;
     SetRendererDrawColor(renderer_, Color(0, 0, 0));
     SDL_RenderClear(renderer_);
-    constexpr int cell_padding = 1;
+    constexpr float cell_padding = 1;
     Minesweeper::FieldView field_view = game_.GetFieldView();
     for (int r = 0; r < field_view.size(); ++r) {
       for (int c = 0; c < field_view[r].size(); ++c) {
@@ -69,20 +109,42 @@ class MinesweeperUI {
             .w = cell_size_ - 2 * cell_padding,
             .h = cell_size_ - 2 * cell_padding,
         };
-        SetRendererDrawColor(renderer_, GetColorForCellView(field_view[r][c]));
-        SDL_RenderFillRect(renderer_, &rect);
+        const Minesweeper::CellView cell_view = field_view[r][c];
+        if (cell_view.type == Minesweeper::CellType::kRevealed &&
+            cell_view.neighbour_mines.has_value() &&
+            cell_view.neighbour_mines.value() > 0) {
+          auto it = number_textures_.find(cell_view.neighbour_mines.value());
+          SDL_RenderTexture(renderer_, it->second, nullptr, &rect);
+        } else {
+          SetRendererDrawColor(renderer_,
+                               GetColorForCellView(field_view[r][c]));
+          SDL_RenderFillRect(renderer_, &rect);
+        }
       }
     }
     SDL_RenderPresent(renderer_);
+  }
+
+  std::optional<std::pair<int, int>> RowAndColFromXY(int x, int y) {
+    int r = x / cell_size_;
+    int c = y / cell_size_;
+    std::pair<int, int> field_size = game_.GetFieldSize();
+    if (r < 0 || r >= field_size.first || c < 0 || c >= field_size.second) {
+      return std::nullopt;
+    }
+    std::pair<int, int> result = std::make_pair(r, c);
+    return result;
   }
 
   void HandleLeftClick(int x, int y) {
     if (game_.IsGameOver()) {
       return;
     }
-    int r = x / cell_size_;
-    int c = y / cell_size_;
-    game_.Reveal(r, c);
+    auto row_col = RowAndColFromXY(x, y);
+    if (!row_col.has_value()) {
+      return;
+    }
+    game_.Reveal(row_col.value().first, row_col.value().second);
     DrawMineField();
   }
 
@@ -90,13 +152,15 @@ class MinesweeperUI {
     if (game_.IsGameOver()) {
       return;
     }
-    int r = x / cell_size_;
-    int c = y / cell_size_;
-    game_.ToggleFlag(r, c);
+    auto row_col = RowAndColFromXY(x, y);
+    if (!row_col.has_value()) {
+      return;
+    }
+    game_.ToggleFlag(row_col.value().first, row_col.value().second);
     DrawMineField();
   }
 
-  Color GetColorForCellView(Minesweeper::CellView cell_view) {
+  Color GetColorForCellView(const Minesweeper::CellView& cell_view) {
     switch (cell_view.type) {
       case Minesweeper::CellType::kHidden:
         // Light grey.
@@ -126,6 +190,8 @@ class MinesweeperUI {
   Minesweeper game_;
   SDL_Window* window_ = nullptr;
   SDL_Renderer* renderer_ = nullptr;
+  TTF_Font* font_ = nullptr;
+  std::unordered_map<int, SDL_Texture*> number_textures_;
 };
 
 }  // namespace minesweeper
@@ -136,6 +202,12 @@ int SDL_AppInit(int argc, char** argv) {
   std::cout << "AppInit" << std::endl;
   int rc = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
   if (rc != 0) {
+    std::cout << "Failed to initialize SDL" << std::endl;
+    return rc;
+  }
+  rc = TTF_Init();
+  if (rc != 0) {
+    std::cout << "Failed to initialize TTF" << std::endl;
     return rc;
   }
   minesweeper_ui = new minesweeper::MinesweeperUI(800, 600, 15);
